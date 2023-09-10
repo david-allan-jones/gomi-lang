f7b2f0cf49104c5dafa46c19b0090693301b0f7f
david-allan-jones <davej1094@yahoo.com>
2023-09-10 19:36:20 +0900

    Remove unneeded code in index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,10 @@
 import { tokenize } from "./src/lexer"
 
-const yargs = require('yargs')
-
-const parsedArgs = yargs(process.argv)
-console.log(parsedArgs)
-
 const filePath = process.argv[2]
 
 const file = Bun.file(filePath)
 const src = await file.text()
 
-console.log(src)
 const tokens = tokenize(src)
 
 console.log(JSON.stringify(tokens))
