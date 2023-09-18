79ea4c204210ff6192de3a48c8d56e40c3a0e041
david-allan-jones <davej1094@yahoo.com>
2023-09-18 16:21:50 +0900

    Refactor interpreter test to make easier to write new tests

--- src/runtime/interpreter.test.ts
+++ src/runtime/interpreter.test.ts
@@ -3,64 +3,50 @@ import { evaluate } from "./interpreter";
 import { ArrayLiteral, BooleanLiteral, Expr, MemberExpr, NilLiteral, IntLiteral, ObjectLiteral, StringLiteral, VarAssignment, VarDeclaration, mk_identifier, mk_int_literal, mk_float_literal } from "../frontend/ast";
 import Scope from "./scope/scope";
 import { fail } from "assert";
-import { IntVal, ObjectVal, VoidVal } from "./types";
+import { ArrayVal, IntVal, ObjectVal, VoidVal } from "./types";
+import GomiParser from "../frontend/parser";
 
 describe('Gomi Interpreter', () => {
+    const runProgram = (src: string, scope: Scope) => {
+        const parser = new GomiParser()
+        const program = parser.produceAST(src)
+        return evaluate(program, scope)
+    }
+
     it('int literal', () => {
-        const stmt = mk_int_literal(1n)
-        const result = evaluate(stmt, new Scope())
+        const result = runProgram('1', new Scope())
         expect(result.type).toBe('int')
         expect(result.value).toBe(1n)
     })
 
     it('float literal', () => {
-        const stmt = mk_float_literal(1)
-        const result = evaluate(stmt, new Scope())
+        const result = runProgram('1.0', new Scope())
         expect(result.type).toBe('float')
         expect(result.value).toBe(1)
     })
 
     it('boolean literals', () => {
-        const stmt = {
-            kind: 'BooleanLiteral',
-            value: true
-        } as BooleanLiteral
-        const result = evaluate(stmt, new Scope())
+        const result = runProgram('true', new Scope())
         expect(result.type).toBe('boolean')
         expect(result.value).toBe(true)
     })
 
     it('string literals', () => {
-        const stmt = {
-            kind: 'StringLiteral',
-            value: 'abc'
-        } as StringLiteral
-        const result = evaluate(stmt, new Scope())
+        const result = runProgram("'abc'", new Scope())
         expect(result.type).toBe('string')
         expect(result.value).toBe('abc')
     })
 
     it('nil literals', () => {
-        const stmt = {
-            kind: 'NilLiteral',
-            value: null
-        } as NilLiteral
-        const result = evaluate(stmt, new Scope())
+        const result = runProgram('nil', new Scope())
         expect(result.type).toBe('object')
         expect(result.value).toBe(undefined)
     })
 
     it('array literals', () => {
-        const stmt = {
-            kind: 'ArrayLiteral',
-            values: [
-                mk_int_literal(1n),
-                mk_int_literal(2n),
-            ]
-        } as ArrayLiteral
-        const result = evaluate(stmt, new Scope())
-        const [val1, val2] = result.value as IntVal[]
+        const result = runProgram('[1, 2]', new Scope())
         expect(result.type).toBe('array')
+        const [val1, val2] = result.value as IntVal[]
         expect(val1.type).toBe('int')
         expect(val1.value).toBe(1n)
         expect(val2.type).toBe('int')
@@ -68,94 +54,38 @@ describe('Gomi Interpreter', () => {
     })
 
     it('object literal', () => {
-        const stmt = {
-            kind: 'ObjectLiteral',
-            props: [
-                {
-                    kind: 'Property',
-                    key: 'a',
-                    value: mk_int_literal(1n)
-                },
-            ]
-        } as Expr
-        const result = evaluate(stmt, new Scope()) as ObjectVal
+        const result = runProgram('{a: 1}', new Scope())
         expect(result.type).toBe('object')
-        const a = result.value?.get('a')
+        const a = (result as ObjectVal).value?.get('a')
         expect(a?.type).toBe('int')
         expect(a?.value).toBe(1n)
     })
 
     it('var declaration', () => {
-        const stmt = {
-            kind: 'VarDeclaration',
-            declarations: [
-                {
-                    identifier: 'a',
-                    value: mk_int_literal(1n)
-                },
-                {
-                    identifier: 'b',
-                    value: mk_int_literal(2n)
-                },
-            ],
-            mutable: true
-        } as VarDeclaration
         const scope = new Scope()
-        const result = evaluate(stmt, scope) as VoidVal
+        const result = runProgram('let a, b = 1, 2;', scope)
         expect(result.type).toBe('void')
         expect(result.value).toBe(null)
-
         expect(scope.lookupVar('a')?.val.value).toBe(1n)
         expect(scope.lookupVar('b')?.val.value).toBe(2n)
     })
 
     it('var assignment', () => {
-        const declaration = {
-            kind: 'VarDeclaration',
-            declarations: [
-                {
-                    identifier: 'a',
-                    value: mk_int_literal(1n)
-                }
-            ],
-            mutable: true
-        } as VarDeclaration
-        const scope = new Scope()
-        evaluate(declaration, scope) as VoidVal
-
-        const stmt = {
-            kind: 'VarAssignment',
-            assignee: mk_identifier('a'),
-            value: mk_int_literal(2n)
-        } as VarAssignment
-
-        const result = evaluate(stmt, scope)
+        const scope = new Scope() 
+        const result = runProgram(`
+            let a = 1;
+            a = 2 
+        `, scope)
         expect(result.type).toBe('int')
         expect(scope.lookupVar('a')?.val.value).toBe(2n)
     })
 
     it('const assignment throws an error', () => {
-        const declaration = {
-            kind: 'VarDeclaration',
-            declarations: [
-                {
-                    identifier: 'a',
-                    value: mk_int_literal(1n)
-                }
-            ],
-            mutable: false
-        } as VarDeclaration
-        const scope = new Scope()
-        evaluate(declaration, scope) as VoidVal
-
-        const stmt = {
-            kind: 'VarAssignment',
-            assignee: mk_identifier('a'),
-            value: mk_int_literal(2n)
-        } as VarAssignment
-
         try {
-            const result = evaluate(stmt, scope)
+            runProgram(`
+                const a = 1;
+                a = 2
+            `, new Scope())
             fail('You were able to assign to a const')
         } catch(e) {
             expect(e).toInclude("'a'")
